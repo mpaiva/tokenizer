936b5719e1a1d0fde33822e2a1aa60afb3e2d3b0
Lucas Rosa <lmrosa007@gmail.com>
2019-03-06 23:37:33 -0500

    feat: add JSON export support

--- src/script.js
+++ src/script.js
@@ -1,51 +1,83 @@
 import sketch from 'sketch'
 
-export const promptSaveLocation = (extension) => {
+export const promptSaveLocation = extension => {
   const saveDialog = NSSavePanel.savePanel()
 
-  saveDialog.setNameFieldStringValue("variables")
+  saveDialog.setNameFieldStringValue('variables')
 
   saveDialog.setAllowedFileTypes([extension])
 
-  saveDialog.setAllowsOtherFileTypes(false);
+  saveDialog.setAllowsOtherFileTypes(false)
 
-  saveDialog.setExtensionHidden(false);
-  
-  // If the users selects 'OK', return the location they specified
-  if (saveDialog.runModal() == NSOKButton)
-    return saveDialog.URL()
+  saveDialog.setExtensionHidden(false)
 
-  // Otherwise return nothing
-  return nil
+  if (saveDialog.runModal() === NSOKButton) return saveDialog.URL()
+
+  return null
 }
 
-export const exportSass = () => {
+const run = (accumulator, extension, foldFunction, stringify = str => str) => {
   const document = sketch.getSelectedDocument()
-  const selectedLayers = document.selectedLayers
+  const { selectedLayers } = document
   const selectedCount = selectedLayers.length
-  
+
   if (selectedCount === 0) {
     sketch.UI.message('No layers are selected. 😕')
   } else {
-    const url = promptSaveLocation("scss")
+    const url = promptSaveLocation(extension)
 
     if (url) {
-      const variables = selectedLayers.reduce((acc, layer) => {
-       if (layer.type === 'Text') {
-         return acc + '$' + layer.name + ': ' + layer.style.fontSize + 'px;\n'
-       } else {
-         return acc + '$' + layer.name + ': ' + layer.style.fills[0].color.slice(0, 7) + ';\n'
-       }
-      }, '')
+      const variables = selectedLayers.reduce(foldFunction, accumulator)
 
-      const path = url.path();
-      const content = NSString.stringWithString(variables);
+      const path = url.path()
+      const content = NSString.stringWithString(stringify(variables))
 
       content.writeToFile_atomically_encoding_error_(
-        path, true, NSUTF8StringEncoding, null
+        path,
+        true,
+        NSUTF8StringEncoding,
+        null
       )
 
-      sketch.UI.message('Sass variables successfully saved. 🎉')
+      sketch.UI.message('Tokens Successfully Exported. 🎉')
     }
   }
+}
+
+const foldLayerToSassVar = (acc, layer) => {
+  if (layer.type === 'Text') {
+    return `${acc}$${layer.name}: ${layer.style.fontSize}px;\n`
+  }
+
+  if (layer.type === 'Rectangle') {
+    return `${acc}$${layer.name}: ${layer.style.fills[0].color.slice(0, 7)};\n`
+  }
+
+  return acc
+}
+
+const foldLayerToJsonKV = (acc, layer) => {
+  if (layer.type === 'Text') {
+    return {
+      ...acc,
+      [layer.name]: `${layer.style.fontSize}px`,
+    }
+  }
+
+  if (layer.type === 'ShapePath') {
+    return {
+      ...acc,
+      [layer.name]: layer.style.fills[0].color.slice(0, 7),
+    }
+  }
+
+  return acc
+}
+
+export const exportSass = () => {
+  run('', 'scss', foldLayerToSassVar)
+}
+
+export const exportJSON = () => {
+  run({}, 'json', foldLayerToJsonKV, JSON.stringify)
 }
--- tokenizer.sketchplugin/Contents/Sketch/script.js
+++ tokenizer.sketchplugin/Contents/Sketch/script.js
@@ -1,168 +1,246 @@
-var that = this;
-function __skpm_run (key, context) {
-  that.context = context;
+var that = this
+function __skpm_run(key, context) {
+  that.context = context
 
-var exports =
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/script.js");
-/******/ })
-/************************************************************************/
-/******/ ({
+  var exports = /******/ (function(modules) {
+    // webpackBootstrap
+    /******/ // The module cache
+    /******/ var installedModules = {} // The require function
+    /******/
+    /******/ /******/ function __webpack_require__(moduleId) {
+      /******/
+      /******/ // Check if module is in cache
+      /******/ if (installedModules[moduleId]) {
+        /******/ return installedModules[moduleId].exports
+        /******/
+      } // Create a new module (and put it into the cache)
+      /******/ /******/ var module = (installedModules[moduleId] = {
+        /******/ i: moduleId,
+        /******/ l: false,
+        /******/ exports: {},
+        /******/
+      }) // Execute the module function
+      /******/
+      /******/ /******/ modules[moduleId].call(
+        module.exports,
+        module,
+        module.exports,
+        __webpack_require__
+      ) // Flag the module as loaded
+      /******/
+      /******/ /******/ module.l = true // Return the exports of the module
+      /******/
+      /******/ /******/ return module.exports
+      /******/
+    } // expose the modules object (__webpack_modules__)
+    /******/
+    /******/
+    /******/ /******/ __webpack_require__.m = modules // expose the module cache
+    /******/
+    /******/ /******/ __webpack_require__.c = installedModules // define getter function for harmony exports
+    /******/
+    /******/ /******/ __webpack_require__.d = function(exports, name, getter) {
+      /******/ if (!__webpack_require__.o(exports, name)) {
+        /******/ Object.defineProperty(exports, name, {
+          /******/ configurable: false,
+          /******/ enumerable: true,
+          /******/ get: getter,
+          /******/
+        })
+        /******/
+      }
+      /******/
+    } // getDefaultExport function for compatibility with non-harmony modules
+    /******/
+    /******/ /******/ __webpack_require__.n = function(module) {
+      /******/ var getter =
+        module && module.__esModule
+          ? /******/ function getDefault() {
+              return module['default']
+            }
+          : /******/ function getModuleExports() {
+              return module
+            }
+      /******/ __webpack_require__.d(getter, 'a', getter)
+      /******/ return getter
+      /******/
+    } // Object.prototype.hasOwnProperty.call
+    /******/
+    /******/ /******/ __webpack_require__.o = function(object, property) {
+      return Object.prototype.hasOwnProperty.call(object, property)
+    } // __webpack_public_path__
+    /******/
+    /******/ /******/ __webpack_require__.p = '' // Load entry module and return exports
+    /******/
+    /******/ /******/ return __webpack_require__((__webpack_require__.s = 0))
+    /******/
+  })(
+    /************************************************************************/
+    /******/ [
+      /* 0 */
+      /***/ function(module, exports, __webpack_require__) {
+        'use strict'
 
-/***/ "./src/script.js":
-/*!***********************!*\
-  !*** ./src/script.js ***!
-  \***********************/
-/*! exports provided: promptSaveLocation, exportSass */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+        Object.defineProperty(exports, '__esModule', {
+          value: true,
+        })
+        exports.exportJSON = exports.exportSass = exports.promptSaveLocation = undefined
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "promptSaveLocation", function() { return promptSaveLocation; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "exportSass", function() { return exportSass; });
-/* harmony import */ var sketch__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! sketch */ "sketch");
-/* harmony import */ var sketch__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(sketch__WEBPACK_IMPORTED_MODULE_0__);
+        var _sketch = __webpack_require__(1)
 
-var promptSaveLocation = function promptSaveLocation(extension) {
-  var saveDialog = NSSavePanel.savePanel();
-  saveDialog.setNameFieldStringValue("variables");
-  saveDialog.setAllowedFileTypes([extension]);
-  saveDialog.setAllowsOtherFileTypes(false);
-  saveDialog.setExtensionHidden(false); // If the users selects 'OK', return the location they specified
+        var _sketch2 = _interopRequireDefault(_sketch)
 
-  if (saveDialog.runModal() == NSOKButton) return saveDialog.URL(); // Otherwise return nothing
-
-  return nil;
-};
-var exportSass = function exportSass() {
-  var document = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.getSelectedDocument();
-  var selectedLayers = document.selectedLayers;
-  var selectedCount = selectedLayers.length;
-
-  if (selectedCount === 0) {
-    sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message('No layers are selected. 😕');
-  } else {
-    var url = promptSaveLocation("scss");
-
-    if (url) {
-      var variables = selectedLayers.reduce(function (acc, layer) {
-        if (layer.type === 'Text') {
-          return acc + '$' + layer.name + ': ' + layer.style.fontSize + 'px;\n';
-        } else {
-          return acc + '$' + layer.name + ': ' + layer.style.fills[0].color.slice(0, 7) + ';\n';
+        function _interopRequireDefault(obj) {
+          return obj && obj.__esModule ? obj : { default: obj }
         }
-      }, '');
-      var path = url.path();
-      var content = NSString.stringWithString(variables);
-      content.writeToFile_atomically_encoding_error_(path, true, NSUTF8StringEncoding, null);
-      sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message('Sass variables successfully saved. 🎉');
-    }
-  }
-};
 
-/***/ }),
+        function _defineProperty(obj, key, value) {
+          if (key in obj) {
+            Object.defineProperty(obj, key, {
+              value: value,
+              enumerable: true,
+              configurable: true,
+              writable: true,
+            })
+          } else {
+            obj[key] = value
+          }
+          return obj
+        }
 
-/***/ "sketch":
-/*!*************************!*\
-  !*** external "sketch" ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+        var promptSaveLocation = (exports.promptSaveLocation = function promptSaveLocation(
+          extension
+        ) {
+          var saveDialog = NSSavePanel.savePanel()
 
-module.exports = require("sketch");
+          saveDialog.setNameFieldStringValue('variables')
 
-/***/ })
+          saveDialog.setAllowedFileTypes([extension])
 
-/******/ });
+          saveDialog.setAllowsOtherFileTypes(false)
+
+          saveDialog.setExtensionHidden(false)
+
+          if (saveDialog.runModal() === NSOKButton) return saveDialog.URL()
+
+          return null
+        })
+
+        var run = function run(accumulator, extension, foldFunction) {
+          var stringify =
+            arguments.length > 3 && arguments[3] !== undefined
+              ? arguments[3]
+              : function(str) {
+                  return str
+                }
+
+          var document = _sketch2['default'].getSelectedDocument()
+          var selectedLayers = document.selectedLayers
+
+          var selectedCount = selectedLayers.length
+
+          if (selectedCount === 0) {
+            _sketch2['default'].UI.message('No layers are selected. 😕')
+          } else {
+            var url = promptSaveLocation(extension)
+
+            if (url) {
+              var variables = selectedLayers.reduce(foldFunction, accumulator)
+
+              var path = url.path()
+              var content = NSString.stringWithString(stringify(variables))
+
+              content.writeToFile_atomically_encoding_error_(
+                path,
+                true,
+                NSUTF8StringEncoding,
+                null
+              )
+
+              _sketch2['default'].UI.message('Tokens Successfully Exported. 🎉')
+            }
+          }
+        }
+
+        var foldLayerToSassVar = function foldLayerToSassVar(acc, layer) {
+          if (layer.type === 'Text') {
+            return (
+              String(acc) +
+              '$' +
+              String(layer.name) +
+              ': ' +
+              String(layer.style.fontSize) +
+              'px;\n'
+            )
+          }
+
+          if (layer.type === 'Rectangle') {
+            return (
+              String(acc) +
+              '$' +
+              String(layer.name) +
+              ': ' +
+              String(layer.style.fills[0].color.slice(0, 7)) +
+              ';\n'
+            )
+          }
+
+          return acc
+        }
+
+        var foldLayerToJsonKV = function foldLayerToJsonKV(acc, layer) {
+          if (layer.type === 'Text') {
+            return Object.assign(
+              {},
+              acc,
+              _defineProperty(
+                {},
+                layer.name,
+                String(layer.style.fontSize) + 'px'
+              )
+            )
+          }
+
+          if (layer.type === 'ShapePath') {
+            return Object.assign(
+              {},
+              acc,
+              _defineProperty(
+                {},
+                layer.name,
+                layer.style.fills[0].color.slice(0, 7)
+              )
+            )
+          }
+
+          return acc
+        }
+
+        var exportSass = (exports.exportSass = function exportSass() {
+          run('', 'scss', foldLayerToSassVar)
+        })
+
+        var exportJSON = (exports.exportJSON = function exportJSON() {
+          run({}, 'json', foldLayerToJsonKV, JSON.stringify)
+        })
+
+        /***/
+      },
+      /* 1 */
+      /***/ function(module, exports) {
+        module.exports = require('sketch')
+
+        /***/
+      },
+      /******/
+    ]
+  )
   if (key === 'default' && typeof exports === 'function') {
-    exports(context);
+    exports(context)
   } else {
-    exports[key](context);
+    exports[key](context)
   }
 }
-that['exportSass'] = __skpm_run.bind(this, 'exportSass');
+that['exportSass'] = __skpm_run.bind(this, 'exportSass')
 that['onRun'] = __skpm_run.bind(this, 'default')
-
-//# sourceMappingURL=script.js.map
+that['exportJSON'] = __skpm_run.bind(this, 'exportJSON')
